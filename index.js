c42f1b0c04760a0011965ac3f37bfe53afecf631
Michael Pope <popemray1986@gmail.com>
2018-06-14 03:39:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ class Driver {
   }
 }
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+}
